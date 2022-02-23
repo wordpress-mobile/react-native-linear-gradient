8dc8d08c3474e0958689cb72743446a629541539
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2022-02-23 13:16:02 +0200

    Build: Update agp to 7.1.1

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,6 +1,6 @@
 pluginManagement {
     plugins {
-        id("com.android.library") version "7.0.4"
+        id("com.android.library") version "7.1.1"
     }
     repositories {
         gradlePluginPortal()
